573696a45422f29ba2437bc4c28e13f6727866ab
sifat hassan <sifat007.sh7@gmail.com>
2021-04-17 11:59:42 +0600

    change package json file and log mongooise connection

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const app = express();
 
 
 
-mongoose.connect('mongodb://localhost:27017/instragram', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/BlogServer', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDb connected");
+});
+
 
 
 app.route('/').get((req,res)=> res.json("hello"))
+
 
 
 app.listen(port, () =>
