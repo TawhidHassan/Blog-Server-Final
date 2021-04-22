be215558b858d9ed84976bdfaca80088230eb92c
sifat hassan <sifat007.sh7@gmail.com>
2021-04-23 02:35:11 +0600

    debug xxx

--- routes/profile.js
+++ routes/profile.js
@@ -4,96 +4,77 @@ const Profile = require("../models/profile.model");
 const middleware = require("../middleware");
 const multer = require("multer");
 const path = require("path");
+
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.decoded.username + ".jpg");
-  },
-});
+    destination: (req, file, cb) => {
+      cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.decoded.username + ".jpg");
+    },
+  });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+  const fileFilter = (req, file, cb) => {
+    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
 
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 6,
-  },
-  // fileFilter: fileFilter,
-});
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 6,
+    },
+    // fileFilter: fileFilter,
+  });
 
+//upload user profile image
 //adding and update profile image
-router.route("/add/image")
-.put(middleware.checkToken, upload.single("img"), (req, res) => {
-    Profile.findOneAndUpdate(
-      { username: req.decoded.username },
-      {
-        $set: {
-          img: req.file.path,
-        },
-      },
-      { new: true },
-      (err, profile) => {
-        if (err) return res.status(500).send(err);
-        const response = {
-          message: "image added successfully updated",
-          data: profile,
-        };
-        return res.status(200).send(response);
-      }
-    );
+router
+  .route("/add/image")
+  .patch(middleware.checkToken, upload.single("img"), (req, res) => {
+    res.send("value");
   });
 
+
+
+
+  //add user profile data
 router.route("/add").post(middleware.checkToken, (req, res) => {
-  const profile = Profile({
-    username: req.decoded.username,
-    name: req.body.name,
-    profession: req.body.profession,
-    DOB: req.body.DOB,
-    titleline: req.body.titleline,
-    about: req.body.about,
-  });
-  profile
-    .save()
-    .then(() => {
-      return res.json({ msg: "profile successfully stored" });
-    })
-    .catch((err) => {
-      return res.status(400).json({ err: err });
+    const profile = Profile({
+      username: req.decoded.username,
+      name: req.body.name,
+      profession: req.body.profession,
+      DOB: req.body.DOB,
+      titleline: req.body.titleline,
+      about: req.body.about,
     });
-});
+
+    profile
+      .save()
+      .then(() => {
+        return res.json({ msg: "profile successfully stored" });
+      })
+      .catch((err) => {
+        return res.status(400).json({ err: err });
+      });
+  });
+
 
 // Check Profile data
 
 router.route("/checkProfile").get(middleware.checkToken, (req, res) => {
-  Profile.findOne({ username: req.decoded.username }, (err, result) => {
-    if (err) return res.json({ err: err });
-    else if (result == null) {
-      return res.json({ status: false, username: req.decoded.username });
-    } else {
-      return res.json({ status: true, username: req.decoded.username });
-    }
-  });
-});
-
-router.route("/getData").get(middleware.checkToken, (req, res) => {
     Profile.findOne({ username: req.decoded.username }, (err, result) => {
       if (err) return res.json({ err: err });
-      if (result == null) return res.json({ data: [] });
-      else return res.json({ data: result });
+      else if (result == null) {
+        return res.json({ status: false, username: req.decoded.username });
+      } else {
+        return res.json({ status: true, username: req.decoded.username });
+      }
     });
-  })
+  });
 
 
-
-
-
-
-module.exports = router;
+  module.exports = router;
